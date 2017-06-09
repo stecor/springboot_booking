338fa242a725bd04ad3f91984d3c83299df6608a
Stefano Corra <stefano.corra@hotmail.com>
2017-06-08 23:29:08 -0400

    Implement a POST, PUT, and DELETE endpoint using Spring MVC 2/2

--- src/main/java/com/booking/study/rest/ReservationResource.java
+++ src/main/java/com/booking/study/rest/ReservationResource.java
@@ -6,6 +6,7 @@ import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -36,5 +37,20 @@ public class ReservationResource {
 							@RequestBody
 							ReservationRequest reservationRequest){
 		return new ResponseEntity<>(new ReservationResponse(), HttpStatus.CREATED);
+	}
+	
+	@RequestMapping(path = "", method = RequestMethod.PUT, produces= MediaType.APPLICATION_JSON_UTF8_VALUE,
+			consumes = MediaType.APPLICATION_JSON_UTF8_VALUE)
+	public ResponseEntity<ReservationResponse> updateReservation(
+							@RequestBody
+							ReservationRequest reservationRequest){
+		return new ResponseEntity<>(new ReservationResponse(), HttpStatus.OK);
+	}
+	@RequestMapping(path = "/{reservationId}", method = RequestMethod.DELETE)
+	public ResponseEntity<Void> deleteReservation(
+								@PathVariable
+								long reservationId){
+		
+		return new ResponseEntity<>(HttpStatus.NO_CONTENT);
 	}
 }
